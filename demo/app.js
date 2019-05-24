e4727d53dd23f6dcb2b710ffe4c980d0a912f6c5
Asa Schachar <asa@optimizely.com>
2019-05-23 18:25:39 -0700

    Update demo to be the right project 7511913042

--- demo/app.js
+++ demo/app.js
@@ -26,7 +26,7 @@ app.use('/webhooks/optimizely', bodyParser.text({ type: '*/*' }), optimizely.web
 app.use('/optimizely/datafile', optimizely.datafileRoute);
 
 app.get('/', function(req, res, next) {
-  const isEnabled = req.optimizely.client.isFeatureEnabled('purchase_option', '123');
+  const isEnabled = req.optimizely.client.isFeatureEnabled('checkout_flow', '123');
   res.render('index', {
     title: 'Express: ' + (isEnabled ? 'feature on!' : 'feature off')
   });
