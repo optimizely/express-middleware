dec3537968f1f331f25295ee610c6ff9ba255d6a
Asa Schachar <asa@optimizely.com>
2019-05-23 17:24:01 -0700

    Move view setup

--- demo/app.js
+++ demo/app.js
@@ -10,6 +10,10 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 /*************
  * Optimizely Express SDK Usage
  */
@@ -21,11 +25,9 @@ app.use(optimizely.middleware);
 app.use('/webhooks/optimizely', bodyParser.text({ type: '*/*' }), optimizely.webhookRequest);
 app.use('/optimizely/datafile', optimizely.datafileRoute);
 
-/*************/
+app.use('/', indexRouter);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/*************/
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,7 +35,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
